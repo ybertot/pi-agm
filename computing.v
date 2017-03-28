80b0dbf8e9f2103f1745c372bccf611d8d50b38b
Yves Bertot <Yves.Bertot@inria.fr>
2017-03-28 14:22:13 +0200

    Adds more examples related to the Brent-Salamin algorithm.

--- computing.v
+++ computing.v
@@ -1,6 +1,7 @@
-Require Import rounding_big.
+Require Import BigZ rounding_big.
 
 Time Eval vm_compute in thousand_digit_pi.
 (* Uncomment the next line for the really big computation. Use a version
   of coq where native_compute works. *)
 (* Time Eval native_compute in  million_digit_pi. *)
+(* Time Eval vm_compute in (salamin (10 ^ 100) 6 - hpi (10 ^ 100) 6)%bigZ. *)
